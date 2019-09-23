dea12f150fd24043391fa01624a9e007e192406c
gregory <degeemon@gmail.com>
2019-09-23 10:21:45 +0800

    implemented ladderconvert
    implemented ladderconvert

--- ian/ladderconvert.py
+++ ian/ladderconvert.py
@@ -3,21 +3,19 @@
 import pandas as pd
 
 ## 
-directory = "/home/bo/PGC/microsat/TestData/Plate1/mini/"
-filename = "A_GUI_12_1.csv"
+test_dir = "/home/bo/PGC/microsat/TestData/Plate1/mini/"
+test_file = "A_GUI_12_1.csv"
 
-def ladder_dataframe(csvdir=directory,file=filename):
+def ladder_dataframe(csvdir=test_dir,file=test_file):
 	dataframe = pd.read_csv(csvdir+file, delimiter=',')
-
-	#Generate table
-	delta = [0]
-	for i in range(len(dataframe[['pos'][0]])):
-	    delta.append(dataframe[['wei'][0]])
 
 	#because i dont know how to iterate through panda columns yet
 	#update when i know how to iterate through panda columns
 
+	#new solution
 
+
+	##works
 	pos = list(dataframe[['pos'][0]])
 	wei = list(dataframe[['wei'][0]])
 	delta = []
@@ -35,14 +33,62 @@ def ladder_dataframe(csvdir=directory,file=filename):
 	deltaframe = pd.DataFrame(list(zip(pos,wei,delta)),columns = ['pos','wei','delta'])
 	return(deltaframe)
 
+# print(ladder_dataframe())	
 
-print(ladder_dataframe())
-
-
-def ladder_bpindex_convert(bp=0,index=0):
-	deltaframe = ladder_dataframe()
+def index_bp(ind = 0,DataFrame=ladder_dataframe()):
 	
+	poslist = list(DataFrame[['pos'][0]])
+	weilist = list(DataFrame[['wei'][0]])
+	dellist = list(DataFrame[['delta'][0]])	
+	print("the index is: " + str(ind))
+	i = 0
+	while i < len(poslist):
+		#check if index is equal to the calibrated ladder
+		#and return the calibrated bp location
+		
+		if ind == poslist[i]:
+			return(ind)
+			#the function ends
+			break
+		#convert using the formula: bp = ((i - pos(n-1))/delta(n))+wei(n-1)
+		elif ind < poslist[i]:
+	
+			bp_location = (((ind - poslist[i-1])/dellist[i]) + weilist[i-1])
+			# print("{} {} {} {}".format(ind,poslist[i-1],dellist[i],weilist[i-1]))
+			return(bp_location)
+			break
+		i+=1
+
+
+print(index_bp(ind=6390))
 
 
 
-	return()
+
+
+
+
+
+# def bpindexconvert(bp=0,index=0):
+# 	#open the data frame
+# 	deltaframe = ladder_dataframe()
+# 	#check if bp or index has values
+# 	#if both has values, return error
+# 	#if bp, convert to index, if index convert to bp
+# 	if bp > 0 & index > 0:
+# 		#print error and break
+# 		return()
+# 	elif bp > 0:
+# 		#convert bp to index
+# 		return()
+# 	elif index > 0:
+# 		#convert index to bp
+# 		return()
+
+
+		
+
+
+
+
+
