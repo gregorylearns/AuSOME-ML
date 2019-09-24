a3637b8612c3e738ad6d158d442cf4c2c29c42fb
gregory <degeemon@gmail.com>
2019-09-24 11:20:18 +0800

    added description for each functions

--- ian/ladderconvert.py
+++ ian/ladderconvert.py
@@ -2,19 +2,19 @@
 #Greg, 9.20.19 4pm
 ## To do: efficiently use pandas instad of lists
 ## To do: support list import for data
-
-
 import pandas as pd
 
-## 
 test_dir = "/home/bo/PGC/microsat/TestData/Plate1/mini/"
 test_file = "A_GUI_12_1.csv"
 
-def ladder_dataframe(csvdir=test_dir,file=test_file):
+def ladder_dataframe(csvdir=test_dir,file=test_file): 
+	"""
+	Creates a dataframe from the CSV file from fragman, and creates a column named delta.
+	Delta is the ratio of the changes in index('pos')/changes in stdard('wei')
+	Delta(ratio) is useful in the next two functions that converts index to bp and vice versa.
+	"""
 	dataframe = pd.read_csv(csvdir+file, delimiter=',')
-	#because i dont know how to iterate through panda columns yet
-	#update when i know how to iterate through panda columns
-	##works
+	#because i dont know how to iterate through panda columns yet && update when I do
 	pos = list(dataframe[['pos'][0]])
 	wei = list(dataframe[['wei'][0]])
 	delta = []
@@ -30,13 +30,14 @@ def ladder_dataframe(csvdir=test_dir,file=test_file):
 	    i +=1
 
 	deltaframe = pd.DataFrame(list(zip(pos,wei,delta)),columns = ['pos','wei','delta'])
-	#returns a column of values which are the ratio of pos and wei.
-	return(deltaframe)
+	return(deltaframe) #returns a table of values that contain 'pos', 'wei', and 'delta'
 
 # print(ladder_dataframe())	
 
-def index_bp(ind,DataFrame=ladder_dataframe()):
-	
+def index_bp(ind,DataFrame):
+	"""
+	Uses the delta value to convert index values into the base pairs value
+	"""
 	poslist = list(DataFrame[['pos'][0]])
 	weilist = list(DataFrame[['wei'][0]])
 	dellist = list(DataFrame[['delta'][0]])	
@@ -61,8 +62,10 @@ def index_bp(ind,DataFrame=ladder_dataframe()):
 
 # print(index_bp(ind=6390))
 
-def bp_index(bp,DataFrame=ladder_dataframe()):
-	
+def bp_index(bp,DataFrame):
+	"""
+	Uses the delta value to convert base pairs values into the index value
+	"""
 	poslist = list(DataFrame[['pos'][0]])
 	weilist = list(DataFrame[['wei'][0]])
 	dellist = list(DataFrame[['delta'][0]])	
@@ -85,9 +88,11 @@ def bp_index(bp,DataFrame=ladder_dataframe()):
 			break
 		i+=1
 
+my_dataframe = ladder_dataframe()
 
-print(bp_index(bp = 206.739))
-print(index_bp(ind=3024 ))
+
+print(bp_index(bp = 206.739,DataFrame=my_dataframe))
+print(index_bp(ind=3024,DataFrame=my_dataframe))
 
 
 ##supposed single function method for all
