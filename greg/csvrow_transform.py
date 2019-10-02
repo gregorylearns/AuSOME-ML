da0a355196b5d77fbdae363f376ce814bc7e369c
gregory <degeemon@gmail.com>
2019-10-02 18:28:18 +0800

    added csvrow_transform

---
+++ greg/csvrow_transform.py
@@ -0,0 +1,40 @@
+#per row
+import pandas as pd
+
+my_dir = "/home/bo/PGC/microsat/testdata/training/GetHeight/"
+filename = "Channel1.csv"
+
+
+def convertrow():
+	
+	file = pd.read_csv(my_dir+filename)
+	i = 0
+	filecol = []
+	peakcol = []
+
+	while i < len(file):
+		if file.iat[i,0] != 'POP':
+			filecol.append(file.iat[i,0])
+			filecol.append(file.iat[i,0])
+			peakcol.append(file.iat[i,1])
+			peakcol.append(file.iat[i,2])
+		else:
+			filecol.append('POP')
+			peakcol.append('')
+		i +=1
+
+	df = pd.DataFrame(list(zip(filecol,peakcol)),columns=["filename","peakcalls"])
+	# print(df)
+	df.to_csv(my_dir+"Channel1_transform.csv")
+	print("Success!")
+
+
+
+
+
+
+
+def main():
+	convertrow()
+
+main()
